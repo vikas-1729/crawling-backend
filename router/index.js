3acad9d67e4d7be8996d0e43f32f34cf2c84e2fe
vikas-1729 <53541816+vikas-1729@users.noreply.github.com>
2020-10-08 12:53:48 +0530

    edit-get

--- router/index.js
+++ router/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/api', require('./api'));
+router.get('/', function (req, res) {
+  return res.status(200).json({
+    message: 'ok',
+  });
+});
 
 module.exports = router;
